803dcb3440f126046fa161e07eae75f3337ffd2e
makar.peter98 <makar.peter98@gmail.com>
2026-02-17 16:44:13 +0100

    handleButtonClick kész

--- app/src/main/java/hu/unideb/mobil/mobil_masodik_ora/MainActivity.java
+++ app/src/main/java/hu/unideb/mobil/mobil_masodik_ora/MainActivity.java
@@ -1,6 +1,10 @@
 package hu.unideb.mobil.mobil_masodik_ora;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultTextview = findViewById(R.id.resultTextView);
+
+    }
+
+    public void handleButtonClick(View view) {
+        Button myButton = (Button) view;
+        String inputString = myButton.getText().toString();
+        Log.d("button", inputString);
+        switch (inputString)
+        {
+            case "=": resultTextview.setText(calculate(resultTextview.getText().toString())); break;
+            case "CE": resultTextview.setText("0"); break;
+            case "-":
+            case "*":
+            case "/": if("+-*/".indexOf(resultTextview.getText().toString().charAt(
+                    resultTextview.getText().toString().length()-1
+            )) > -1 ) break;
+            default: resultTextview.append(myButton.getText());
+        }
+    }
+    public String calculate(String expression)
+    {
+        return "NaN";
     }
 }
